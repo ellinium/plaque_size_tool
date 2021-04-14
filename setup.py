c243841c6f80b2fdde3b1d8b18f0c2e0bdbadf8f
ellinium <ellina.trofimova@gmail.com>
2021-04-14 22:49:39 +1000

    version 1.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='plaque-size-tool',
-      version='1.0.7',
+      version='1.0.8',
       description='Bacteriophage plaque size measurement tool',
       long_description=long_description,
       long_description_content_type='text/markdown',
